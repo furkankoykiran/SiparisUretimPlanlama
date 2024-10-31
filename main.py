c94fcd3cab8923957b7066468f02b17a1f2182f4
Furkan Köykıran <furkankoykiran@gmail.com>
2024-10-31 20:40:54 +0300

    Add main menu functionality

---
+++ main.py
@@ -0,0 +1,31 @@
+from utils.Databases.Sirket import SirketDB
+from models.urun import Urun
+from models.siparis import Siparis
+from models.calisan import Calisan
+
+def main_menu():
+    sirket_db = SirketDB('db/sirket.db')
+    sirket_db.create_table()
+
+    while True:
+        print("""
+        1. Ürün İşlemleri
+        2. Çalışan İşlemleri
+        3. Sipariş İşlemleri
+        4. Çıkış
+        """)
+        secim = input("Bir seçim yapın: ")
+
+        if secim == '1':
+            Urun.menu(sirket_db)
+        elif secim == '2':
+            Calisan.menu(sirket_db)
+        elif secim == '3':
+            Siparis.menu(sirket_db)
+        elif secim == '4':
+            break
+        else:
+            print("Geçersiz seçim. Tekrar deneyin.")
+
+if __name__ == "__main__":
+    main_menu()
