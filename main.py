548cd8649c95227ef62c1911ff5d6bba5f9f4864
Furkan Köykıran <furkankoykiran@gmail.com>
2025-01-02 12:34:48 +0300

    Add Raporlar menu option to main menu for report generation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from utils.Databases.Sirket import SirketDB
 from models.urun import Urun
 from models.siparis import Siparis
 from models.calisan import Calisan
+from models.raporlar import Raporlar  # Raporlar menüsü için eklendi
 
 def main_menu():
     sirket_db = SirketDB('db/sirket.db')
@@ -12,7 +13,8 @@ def main_menu():
         1. Ürün İşlemleri
         2. Çalışan İşlemleri
         3. Sipariş İşlemleri
-        4. Çıkış
+        4. Raporlar
+        5. Çıkış
         """)
         secim = input("Bir seçim yapın: ")
 
@@ -23,6 +25,8 @@ def main_menu():
         elif secim == '3':
             Siparis.menu(sirket_db)
         elif secim == '4':
+            Raporlar.menu(sirket_db)
+        elif secim == '5':
             break
         else:
             print("Geçersiz seçim. Tekrar deneyin.")
